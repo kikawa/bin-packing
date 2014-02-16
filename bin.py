1c872e6a146711d879c8680084b888b70367fa40
kikawa <kikawadev@hushmail.com>
2014-02-16 13:02:58 +0000

    Modified bin class. Added 'show' method.

--- bin.py
+++ bin.py
@@ -2,4 +2,19 @@
 
 class Bin:
 	def __init__(self):
-		
+		self.list = []
+
+	def addItem(self, item):
+		self.list.append(item)
+
+	def removeItem(self, item):
+		self.list.remove(item)
+
+	def sum(self):
+		total = 0
+		for elem in self.list:
+			total += elem
+		return total
+
+	def show(self):
+		return self.list
